7ff0a5d757106f9d6d72bc2211614437213debbd
Jeremy Badger <jeremebadger@gmail.com>
2023-04-26 17:12:11 -0700

    Printing names of bands.
    Added logging.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace RhythmsGonnaGetYou
 
             var bandCount = context.Bands.Count();
             Console.WriteLine($"There are {bandCount} bands!");
+
+            foreach (var band in context.Bands)
+            {
+                Console.WriteLine($"There is a band named {band.Name}.");
+            }
         }
     }
 }
